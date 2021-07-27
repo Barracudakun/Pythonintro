b05c6e1c282cf49cee5798c7c868a9ef1efa15f9
Yingxu Yao <yingxuyao@fb.com>
2021-07-27 14:43:59 -0500

    new update

--- lesson_4/homework_4_1.py
+++ lesson_4/homework_4_1.py
@@ -18,36 +18,48 @@ print(result_3)
 # Slice string string_3 from 5th to 20th (excluding 20th) character and save the result to variable result_4
 
 string_3 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_4 = None
+result_4 = string_3[5:20]
+print(result_4)
 
 
 # Slice string string_4 from 10th character to the end of the string. Save only every second character to variable
 # result_5
 
 string_4 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_5 = None
-
+result_5 = string_4[10: :2]
+print(result_5)
 
 # Slice string string_5 from the first to the last character, save only every forth character and
 # save the result to variable result_6
 
 string_5 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_6 = None
+result_6 = string_5[ : :4]
+print(result_6)
 
 
 # Slice string string_6 from the first to 14th (including 14th) character, save only every third character and save
 # the result to variable result_7
 
 string_6 = 'Python is a programming language that lets you work quickly and integrate systems more effectively'
-result_7 = None
+result_7 = string_6[0:15:3]
+print(result_7)
 
 
 # Save to variable result_8 string value from string_7 variable, written in reverse order, using slicing.
 
 string_7 = 'Python'
-result_8 = None
+result_8 = string_7[ :: ]
+print(result_8)
 
 
 # Create a range of numbers from 0 to 10 (excluding 10) and save it to result_9 variable
+#
+# for result_9 in range(0,10):
+#     print(result_9, end="\t")
 
-result_9 = None
+
+for i in range(0, 10):
+i = result_9
+
+
+
--- lesson_4/homework_4_2.py
+++ lesson_4/homework_4_2.py
@@ -3,8 +3,15 @@
 # Ex. 1
 # Enter your name, save it in name variable and save in result_1 variable your name repeated 3 times (use loops)
 
-name_1 = None
-result_1 = None
+name_1 = "yingxuyao"
+result_1 = name_1
+for i in range(0, 3):
+    print(result_1)
+
+i = 1
+while i <= 3:
+    print(result_1)
+    i += 1
 
 # TODO: Here is your code
 
@@ -36,3 +43,5 @@ result_3 = None
 result_4 = None
 
 # TODO: Here is your code
+
+
