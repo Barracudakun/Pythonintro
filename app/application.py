a2336764051a1e951ae63a7912227199de7393eb
Yingxu Yao <yingxuyao@fb.com>
2021-08-02 12:03:29 -0500

    Aug 2 Application

---
+++ app/application.py
@@ -0,0 +1,15 @@
+from pages.header import Header
+from pages.main_page import Main
+from pages.product_page import ProductPage
+from pages.search_results_page import SearchResults
+
+
+class Application:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+        self.main_page = Main(self.driver)
+        self.header = Header(self.driver)
+        self.product_page = ProductPage(self.driver)
+        self.search_results_page = SearchResults(self.driver)
